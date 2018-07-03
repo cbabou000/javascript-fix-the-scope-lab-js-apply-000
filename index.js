ee470b1a071c83c35672d90162a320b9bd43e572
bbabou94@gmail.com <bbabou94@gmail.com>
2018-07-03 20:58:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,9 +22,9 @@ function add2(n) {
 }
 
 var funkyFunction = function theFunk(string){
-    return theFunk = "FUNKY!";
+    return theFunk = "FUNKY!";{
   }
-
+}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
